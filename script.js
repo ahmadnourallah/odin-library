59d0a3118c65d7e72b0e0496df58497f9992ed3f
Ahmad Nourallah <ahmadnurallah@gmail.com>
2024-11-22 03:55:02 -0500

    Cread loading books functionality

--- script.js
+++ script.js
@@ -18,4 +18,22 @@ Book.prototype.toggleStatus = function () {
 function addBook(title, author, pages, status) {
     let book = new Book(title, author, pages, status);
     myLibrary.push(book);
+    loadBooks();
+}
+
+function loadBooks() {
+    myLibrary.forEach((book, index) => {
+        let card = document.createElement("div");
+        card.classList.add("card", "shadow");
+        card.dataset.index = index;
+        card.innerHTML = `
+            <div class="title">${book.title}</div>
+            <div class="author">${book.author}</div>
+            <div class="pages">${book.pages} pages</div>
+            <button class="status" data-read="${book.status}">${book.status ? "Read" : "Not read"}</button>
+            <button class="remove">Remove</button>
+        `;
+
+        cardContainer.append(card);
+    });
 }
